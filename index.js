9caab8603fd43a8eb951e7ac5483754f1671c2f5
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-01-03 16:59:37 +0000

    Add "Enter search" functionality

---
+++ index.js
@@ -0,0 +1,42 @@
+var $searchInput = $('#search-input');
+var $searchButton = $('#search-button');
+var $randomButton = $('#random-button');
+var $searchResults = $('#search-results');
+
+function generateURL() {
+  return 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&callback=?&gsrsearch=' + $searchInput.val();
+};
+
+function generateResult(result) {
+  var $row = $('<li class="row result-row" />');
+  var $text = $('<div class="col-sm-10" />');
+  var $title = $('<h1 />', {text: result.title});
+  var $extract = $('<p />', {text: result.extract});
+  $text.append($title);
+  $text.append($extract);
+  $row.append($text);
+
+  var $thumbnail = $('<div class="col-sm-2" />');
+  var $img = $('<img />', {
+    class: 'result-thumbnail',
+    src: result.thumbnail.source
+  });
+  $thumbnail.append($img);
+  $row.append($thumbnail);
+
+  return $row;
+}
+
+function getData() {
+  $searchResults.empty();
+  $.getJSON(generateURL()).then(function(data) {
+    var pages = data.query.pages;
+    Object.keys(pages).forEach(function(key) {
+      $searchResults.append(generateResult(pages[key]));
+    });
+  });
+}
+
+$searchButton.on('click', function() {
+  getData();
+});
